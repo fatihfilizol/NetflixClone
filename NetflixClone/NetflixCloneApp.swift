312aa27b2e09dbbe6bc1f4d3c3dfdecc15d42e96
fatihfilizol <fatihfilizol@gmail.com>
2022-12-31 19:38:52 +0300

    Initial Commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Fatih Filizol on 31.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
